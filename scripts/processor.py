2a0bd0fcb9c18c15d3f675b83fe3e196c8fa990a
Tomasz Jurtsch <tomasz.j@protonmail.com>
2019-03-30 18:02:15 +0100

    Rotate working coordinate system to match plane of the table

--- scripts/processor.py
+++ scripts/processor.py
@@ -17,6 +17,14 @@ def alcohol_latency(speeds):
         return latency_queue.pop(0)
     return Vector3()
 
+def rotate_vector(v):
+    rvec = Vector3()
+    rvec.x = 0.48808 * v.x + 0.24668 * v.y + 0.05319  * v.z
+    rvec.y = 0.1657  * v.x + 0.28382 * v.y + 0.049499 * v.z
+    rvec.z = 0.5203  * v.x + 0.67565 * v.y + 0.06884  * v.z
+
+    return rvec
+
 # latency_queue = []
 # latency_queue_len = 100
 # 
@@ -44,6 +52,7 @@ if __name__ == '__main__':
     rate = rospy.Rate(100)
     while not rospy.is_shutdown():
         speeds_processed = alcohol_latency(speeds)
+        speeds_processed = rotate_vector(speeds_processed)
         pub.publish(speeds_processed)
         rate.sleep()
 
