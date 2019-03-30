ffc1130a8df567442ba07809b4f1f1f6460ac6ed
Maciej Stanek <makaron28@gmail.com>
2019-03-30 16:27:04 +0100

    first commit

---
+++ scripts/processor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+
+from std_msgs.msg import Int32
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Vector3
+
+latency_queue = []
+latency_queue_len = 100
+
+def alcohol_latency(speeds):
+    global latency_queue
+    global latency_queue_len
+    latency_queue.append(speeds)
+    if len(latency_queue) > latency_queue_len:
+        return latency_queue.pop(0)
+    return Vector3()
+
+latency_queue = []
+latency_queue_len = 100
+
+def alcohol_slide(speeds):
+    global slide_queue
+    global slide_queue_len
+    slide_queue.append(speeds)
+    if len(latency_queue) > latency_queue_len:
+        latency_queue.pop(0)
+    # x = [i for i in 
+
+speeds = Vector3()
+
+def fnc_callback(msg):
+    global speeds
+    speeds.x = msg.axes[3]
+    speeds.y = msg.axes[4]
+    speeds.z = msg.axes[1]
+    speeds = alcohol_latency(speeds)
+    # speeds = alcohol_slide(speeds)
+   
+if __name__ == '__main__':
+    rospy.init_node('processor')
+    pub=rospy.Publisher('speeds', Vector3, queue_size=1)
+    sub=rospy.Subscriber('joy', Joy, fnc_callback)
+
+    rate = rospy.Rate(100)
+    while not rospy.is_shutdown():
+        pub.publish(speeds)
+        rate.sleep()
+
+
